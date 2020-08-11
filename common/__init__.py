4ffd38a5a9f0eab1953d92bef3e4dbbc1d247e13
ukoCuon <uko@cuon.co.jp>
2020-08-11 14:21:22 +0900

    add mnist fa

---
+++ common/__init__.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+from tensorflow import keras as keras
+
+import numpy as np
+import matplotlib.pyplot as plt
---
+++ mnist_fashion/mnist_fashion.py
@@ -0,0 +1,15 @@
+# import tensorflow as tf
+# from tensorflow import keras as keras
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+import __init__
+
+import pdb; pdb.set_trace()
+# learning
+  # load input data
+  # create network
+  # learning
+  # output with images
+# predict
